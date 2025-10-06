4bd0137dce7ca9a0cd4bb575c8233b4c2abc587e
Gurucharan S <charansuriya2000@gmail.com>
2025-10-06 18:56:39 +0530

    Defining Pipeline operations

--- beam_config.py
+++ beam_config.py
@@ -17,5 +17,64 @@ options = PipelineOptions(pipeline_args)
 
 p = beam.Pipeline(options=options)
 
+def remove_last_column(row):
+    cols= row.split(',')
+    item = str(cols[4])
+
+    if item.endswith(':'):
+        cols = item[:-1]
+
+    return ','.join(cols[:-1])
+
+def remove_special_characters(row):
+    import re
+    cols = row.split(',')
+    ret= ''
+    for col in cols:
+        clean_col = re.sub(r'[?%&]','', col)
+        ret = ret + clean_col + ','
+    ret = ret[:-1]
+    return ret
+    
+def print_row(row):
+    print(row)
+
 cleaned_data = (
-    p)
+    p
+    | beam.io.ReadFromText(inputs_pattern, skip_header_lines=1)
+    | beam.Map(remove_last_column)
+    | beam.Map(lambda row: row.lower())
+    | beam.Map(remove_special_characters)
+    | beam.Map(lambda row: row+',1')
+)
+
+delivered_orders = (
+	cleaned_data
+	| 'delivered filter' >> beam.Filter(lambda row: row.split(',')[8].lower() == 'delivered')
+
+)
+
+other_orders = (
+    cleaned_data
+    | 'Undelivered Filter' >> beam.Filter(lambda row: row.split(',')[8].lower() != 'delivered')
+)
+
+(cleaned_data
+ | 'count total' >> beam.combiners.Count.Globally() 		# 920
+ | 'total map' >> beam.Map(lambda x: 'Total Count:' +str(x))	# Total Count: 920
+ | 'print total' >> beam.Map(print_row)
+
+)
+
+(delivered_orders
+ | 'count delivered' >> beam.combiners.Count.Globally()
+ | 'delivered map' >> beam.Map(lambda x: 'Delivered count:'+str(x))
+ | 'print delivered count' >> beam.Map(print_row)
+ )
+
+
+(other_orders
+ | 'count others' >> beam.combiners.Count.Globally()
+ | 'other map' >> beam.Map(lambda x: 'Others count:'+str(x))
+ | 'print undelivered' >> beam.Map(print_row)
+ )
